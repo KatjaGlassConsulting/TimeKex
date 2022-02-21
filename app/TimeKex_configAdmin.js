f7df98bba7db8d3f6a7380d051aee698a9393134
Katja Glaß <katja.glass@glacon.eu>
2022-02-21 11:12:26 +0100

    Deployed 751ec58 with MkDocs version: 1.1.2

---
+++ app/TimeKex_configAdmin.js
@@ -0,0 +1,7 @@
+var config = {
+    "username" : "susan_super",
+    "password" : "api_kitten",
+    "kimaiAPI" : "https://demo-stable.kimai.org/api/",
+    "ignoreActivities" : ["Vacation","Public Holiday"],
+    "adminUser" : ["anna_admin","susan_super"]
+}
