5e9ee202bdba0510fcff82138305fe6e1b1a568d
Katja Glass <katja.glass@glacon.eu>
2024-01-07 11:38:26 +0100

    Deployed b0fca35 with MkDocs version: 1.1.2

---
+++ app/config.js
@@ -0,0 +1,9 @@
+var config = {
+    "kimaiAPI" : "https://demo.kimai.org/api/",
+    "ignoreActivities" : ["Vacation","Public Holiday"],
+    "adminUser" : ["anna_admin","susan_super"],
+    "approval" : false,
+    "overtime" : false,
+    "summary15mins" : false,
+    "breakChecks" : false
+}
